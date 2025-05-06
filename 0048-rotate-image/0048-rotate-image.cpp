7c7a7401e6f8f2eca745605895154158b9712a1c
Rupakkumar <81298410+rupak1005@users.noreply.github.com>
2025-05-06 22:39:26 +0530

    0048-ROTATE-IMAGE Stats: Time: 0 ms (100%), Space: 10.1 MB (68.45%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -5,7 +5,7 @@ public:
         
 
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
+            for(int j=0;j<i;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
