670174fc64092a66be972a01d4d936b92de76893
Rupakkumar <81298410+rupak1005@users.noreply.github.com>
2025-05-07 22:04:37 +0530

    0229-MAJORITY-ELEMENT-II Stats: Time: 0 ms (100%), Space: 23.6 MB (11.91%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,34 +3,35 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> ans;
         int n = nums.size();
-        int cnt1=0;
-        int cnt2=0;
-        int el1=INT_MIN;
-        int el2=INT_MIN;
-        for(auto i:nums){
-            if(cnt1==0 && el2!=i){
-                cnt1=1;
-                el1=i;
-            }else if(cnt2==0 && el1!=i){
-                cnt2=1;
-                el2=i;
-            }else if(el1==i)cnt1++;
-            else if(el2==i)cnt2++;
-            else{
-                cnt1--,cnt2--;
+        
+        int cnt1 = 0, cnt2 = 0;
+        int el1 = 0, el2 = 1;  // initialize to different values
+        
+        for (auto i : nums) {
+            if (el1 == i) cnt1++;
+            else if (el2 == i) cnt2++;
+            else if (cnt1 == 0) {
+                el1 = i;
+                cnt1 = 1;
+            } else if (cnt2 == 0) {
+                el2 = i;
+                cnt2 = 1;
+            } else {
+                cnt1--;
+                cnt2--;
             }
         }
-
-         cnt1=0;
-         cnt2=0;
-        for(auto i:nums){
-            if(el1==i)cnt1++;
-            if(el2==i)cnt2++;
-        }
-        if(cnt1>n/3)ans.push_back(el1);
-        if(cnt2>n/3)ans.push_back(el2);
-        return ans;
-
         
+        // Verify counts
+        cnt1 = cnt2 = 0;
+        for (auto i : nums) {
+            if (i == el1) cnt1++;
+            else if (i == el2) cnt2++;
+        }
+        
+        if (cnt1 > n / 3) ans.push_back(el1);
+        if (cnt2 > n / 3 && el2 != el1) ans.push_back(el2);
+        
+        return ans;
     }
 };
