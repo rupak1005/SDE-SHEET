83605cf80feba74277287bc8c620a9463fde851b
Rupakkumar <81298410+rupak1005@users.noreply.github.com>
2025-05-07 21:01:36 +0530

    0050-POWX-N Stats: Time: 0 ms (100%), Space: 8.6 MB (44.68%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     double myPow(double x, int n) {
         double ans=1.0;
-        long long m=n;
-        if(m<0)m=-1*m;
+        long long m=n;//to overcome overflow issue
+        if(m<0)m=-1*m;//to make it positive first then will invert the answer
         while(m>0){
-            if(m%2==1){
-                ans*=x;
-                m--;
-            }else{
+            if(m%2==1){//if power odd
+                ans*=x;//multiply x to the ans
+                m--;//minus the power
+            }else{//if even
                 x*=x;
-                m/=2;
+                m/=2;//half the power
             }
             
         }
-        if(n<0){
+        if(n<0){//invert if -ve
                 ans=1.0/ans;
             }
         return ans;
