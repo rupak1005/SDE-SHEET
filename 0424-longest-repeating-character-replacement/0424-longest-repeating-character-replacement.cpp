64c87d9432a6cd9d7c3d15630026405690e9d3b2
Rupakkumar <81298410+rupak1005@users.noreply.github.com>
2025-10-16 13:08:34 +0530

    0424-LONGEST-REPEATING-CHARACTER-REPLACEMENT Stats: Time: 7 ms (44.57%), Space: 10.9 MB (40.17%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        vector<int> Occurance(26,0);
-        int left=0,right=0,maxOccurance=0,ans=0;
-
-        while(right<s.length()){
-            maxOccurance=max(maxOccurance,++Occurance[s[right]-'A']);
-            if((right-left+1)-maxOccurance>k){
-                Occurance[s[left]-'A']--;
-                left++;
-            }
-            ans=max(ans,right-left+1);
-            right++;
-
+       int left=0,right=0,maxOccurance=0,ans=0;
+       vector<int>Occurance(26,0);
+       while(right<s.length()){
+        maxOccurance=max(maxOccurance,++Occurance[s[right]-'A']);
+        if((right-left+1)-maxOccurance>k){
+            Occurance[s[left]-'A']--;
+            left++;
         }
-        return ans;
+        ans=max(ans,right-left+1);
+        right++;
+
+       }
+       return ans;
         
     }
 };
