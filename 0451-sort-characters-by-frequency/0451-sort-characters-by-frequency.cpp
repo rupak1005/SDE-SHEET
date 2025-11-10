1057ffc9e9a16891e1c8b69d1ced315ae17fa928
Rupakkumar <81298410+rupak1005@users.noreply.github.com>
2025-11-10 13:35:02 +0530

    0451-SORT-CHARACTERS-BY-FREQUENCY Stats: Time: 0 ms (100%), Space: 11.4 MB (40.34%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
     static bool custom(pair<char,int>&a,pair<char,int> &b){
-        if(a.second==b.second)return a.first<b.first;
+        // if(a.second==b.second)return a.first>b.first;
 
         return a.second>b.second;
     }
