77bfcfd39e7f870a5dc5117e9b38e4af6e2f6850
Rupakkumar <81298410+rupak1005@users.noreply.github.com>
2026-02-23 18:51:34 +0530

    0088-MERGE-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 12.2 MB (71.76%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=0;i<n;i++){
-            nums1[i+m]=nums2[i];
+        for(int i=m;i<m+n;i++){
+            nums1[i]=nums2[i-m];
         }
-        
         sort(nums1.begin(),nums1.end());
+        
     }
-    };
+};
