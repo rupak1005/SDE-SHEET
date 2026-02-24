419a638eb37530663af4bd86fec82b570f66a9fb
Rupakkumar <81298410+rupak1005@users.noreply.github.com>
2026-02-24 14:53:06 +0530

    0056-MERGE-INTERVALS Stats: Time: 5 ms (52.79%), Space: 23.9 MB (31.28%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
-        for(auto i :intervals){
-            if(ans.empty() || i[0]>ans.back()[1]){
-                ans.push_back(i);
+        ans.push_back(intervals[0]);
+
+        for(int i=1;i<intervals.size();i++){
+            vector<int>& previnterval=ans.back();
+
+            if(previnterval[1]>=intervals[i][0]){
+                previnterval[1]=max(previnterval[1],intervals[i][1]);
             }else{
-                ans.back()[1]=(max(ans.back()[1],i[1]));
+                ans.push_back(intervals[i]);
             }
         }
         return ans;
